7af34d24cef27134827d0fbed481d13f558dbcf5
Dengel <52581244+DengelLionel@users.noreply.github.com>
2022-09-03 17:48:54 -0500

    Correccion de header

--- src/css/Header/HeaderStyle.js
+++ src/css/Header/HeaderStyle.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 export const HeaderStyle=styled.header`
-height:14.7vh;
+height:13vh;
 
 background:	${props=>(props.fondoheader)};
 border-bottom-left-radius:40px;
@@ -8,7 +8,7 @@ border-bottom-right-radius:40px;
 position:${ props=>(props.scrollVertical<=40?"relative" :"fixed")};
 z-index:${props=>(props.scrollVertical<100?"70":"70")};
 width:100%;
-bottom:${props=>(props.scrollVertical===0?"27.5vh" :"85.3vh")};
+bottom:${props=>(props.scrollVertical===0?"23vh" :"85.3vh")};
 transition:position bottom 5s;
 @media only screen and (min-width: 640px){
     border-bottom-left-radius:0px;
